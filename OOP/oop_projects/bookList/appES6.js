acb5f8ea1d5c0256763be18ec60e8d3f9ccfd4ec
iMac <svelasco5@student.rccd.edu>
2018-12-16 22:47:44 -0800

    working with ES6

--- OOP/oop_projects/bookList/appES6.js
+++ OOP/oop_projects/bookList/appES6.js
@@ -3,7 +3,7 @@
 */
 
 //Book class
-class BookList {
+class Book {
   constructor(title,author,isbn) {
     this.title = title;
     this.author = author;
@@ -13,8 +13,37 @@ class BookList {
 
 //UI class
 class UI {
-  constructor() {
-
+  addBookToList(book){
+    //grab the table body
+    const list = document.querySelector('tbody');
+    //create a row element
+    const row = document.createElement('tr');
+    //add child element data to the row element
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a class="delete" href="#">X</a></td>
+    `;
+    //appending the row to the list
+    list.appendChild(row);
+  }
+  clearFields(){
+    document.querySelector('input[name="title"]').value = '';
+    document.querySelector('input[name="author"]').value = '';
+    document.querySelector('input[name="isbn"]').value = '';
+  }
+  alertMsg(message,status){
+    const parentElement = document.querySelector('.container'); //parentElement
+    const list = document.querySelector('#book-list'); //grabbing element to insert before this
+    const alertBox = document.createElement('div');
+    alertBox.className = `${status} u-full-width`;
+    alertBox.appendChild(document.createTextNode(message));
+    parentElement.insertBefore(alertBox,list);
+    setTimeout(function(){document.querySelector(`.${status}`).remove()},1500);
+  }
+  removeBook(target){
+    target.parentElement.parentElement.remove();
   }
 }
 
@@ -52,3 +81,40 @@ class Store {
     localStorage.setItem('books',JSON.stringify(books));
   }
 }
+
+
+
+
+//EVENT LISTENERS
+const form = document.querySelector('#book-form');
+let title,author,isbn; //declaring outside of vent listener so that arent declared every time;
+form.addEventListener('submit',function(e){
+  //Form values title,author, ISBN#
+  title = document.querySelector('input[name="title"]').value;
+  author = document.querySelector('input[name="author"]').value;
+  isbn = document.querySelector('input[name="isbn"]').value;
+
+  //make UI object and update the User Interface
+  const ui = new UI();
+  //check for validation
+  if (title == '' || author == '' || isbn == '') {
+    ui.alertMsg('check values','error');
+  } else {
+    //Grab form values ; make book object
+    const book = new Book(title,author,isbn)
+    ui.addBookToList(book); //adding book to list
+    ui.clearFields(); //clearing the form fields
+    ui.alertMsg('Book added successfully','success');
+  
+    //prevent default of a form submittal
+    e.preventDefault();
+  }
+});
+
+//Remove book
+document.querySelector('#book-list').addEventListener('click',function(e){
+  if(e.target.classList.contains('delete')){
+    const ui = new UI();
+    ui.removeBook(e.target);
+  }
+});
