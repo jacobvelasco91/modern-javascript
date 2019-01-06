70442beb323be0d3b59371360a7f6291fd5bd1bc
jacobvelasco <jacobvelasco91@gmail.com>
2019-01-05 21:04:20 -0800

    working on OOP

--- OOP/oop_projects/todo_list/app.js
+++ OOP/oop_projects/todo_list/app.js
@@ -10,6 +10,7 @@ class UI {
     const list = document.querySelector('ul');
     const li = document.createElement('li');
     const a = document.createElement('a');
+    a.className = 'delete';
     a.setAttribute('title','delete');
     a.setAttribute('href','#');
     a.innerHTML = '<i class="far fa-trash-alt"></i>';
@@ -17,17 +18,44 @@ class UI {
     li.appendChild(a);
     list.appendChild(li);
   }
+  deleteTask(target){
+    target.parentElement.parentElement.remove();
+  }
+  deleteAll(){
+    let list = document.querySelector('.task_list_box ul');
+    let lis = document.querySelectorAll('li');
+    console.log(lis.firstChild);
+  }
 }
 
 
+const inputform = document.querySelector('#form');
+let taskValue;
+const ui = new UI();
+let list = document.querySelector('.task_list_box');
 
+/* EVENT LISTENERS */
 
-let inputForm = document.querySelector('#form');
-//listen for form submital
-inputForm.addEventListener('submit',function(e){
-  let input = document.querySelector('input[name="task"]').value;
-  const task = new Task(input);
-  const ui = new UI();
+// form/input
+inputform.addEventListener('submit',function(e){
+  taskValue = document.querySelector('input[name="task"]').value;
+  const task = new Task(taskValue);
   ui.displayTasks(task);
+
   e.preventDefault();
+});
+
+//delete button
+list.addEventListener('click',function(e){
+  if (e.target.parentElement.classList.contains('delete')) {
+   const ui = new UI();
+   ui.deleteTask(e.target);
+  }
+  console.log(e.target);
+});
+
+//clear all tasks
+document.querySelector('.delete_all').addEventListener('click',function(e){
+  const ui = new UI();
+  ui.deleteAll();
 });
